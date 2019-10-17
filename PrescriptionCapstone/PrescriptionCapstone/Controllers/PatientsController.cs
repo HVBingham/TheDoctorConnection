28c4dad7e1818813625bf4df7281141764acdddc
Hannah Bingham <binghan.hb@gmail.com>
2019-10-17 13:30:00 -0500

    Adding to the  Patient controller for create.

--- PrescriptionCapstone/PrescriptionCapstone/Controllers/PatientsController.cs
+++ PrescriptionCapstone/PrescriptionCapstone/Controllers/PatientsController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using PrescriptionCapstone.Models;
 
 namespace PrescriptionCapstone.Controllers
@@ -57,6 +58,8 @@ namespace PrescriptionCapstone.Controllers
         {
             if (ModelState.IsValid)
             {
+                var user = User.Identity.GetUserId();
+                patient.UserId = user;
                 context.Patients.Add(patient);
                 context.SaveChanges();
                 return RedirectToAction("Index");
